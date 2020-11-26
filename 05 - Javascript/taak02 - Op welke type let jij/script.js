e8f04a079d5466a848661fe864b26260f637f1ae
ImranKaouch <2122928@talnet.nl>
2020-11-26 11:20:34 +0100

    Java script

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,14 @@
+let mijnVoornaam="Imran";
+let mijnAchternaam="Kaouch";
+let leeftijd="16";
+let ikBenStemGerechtig="False";
+let ikMagLegaalAlcoholKopen="False";
+let ikMagBrommerRijden="True";
+let SoftwareOntwikkeling="Mijn opleiding";
+console.log(mijnVoornaam);
+console.log(mijnAchternaam);
+console.log(leeftijd);
+console.log(ikBenStemGerechtig);
+console.log(ikMagLegaalAlcoholKopen);
+console.log(ikMagBrommerRijden);
+console.log(SoftwareOntwikkeling); 
