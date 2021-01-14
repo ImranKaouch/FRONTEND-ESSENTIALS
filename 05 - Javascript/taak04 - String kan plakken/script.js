05e0fdd79d2c31b0f203f53e90efc91843f0c86e
ImranKaouch <2122928@talnet.nl>
2021-01-14 16:26:15 +0100

    taak 04

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,9 @@
+let student= {
+    mijnVoornaam:"Imran",
+    mijnAchternaam: "Kaouch",
+    mijnLeeftijd: "16"
+}
+console.log(student);
+
+let volledigeNaam= "Mijn voornaam is "+student.mijnVoornaam+" "+ student.mijnAchternaam+ "en mijn leeftijd is "+student.mijnLeeftijd+ " jaar.";
+console.log (volledigeNaam);
