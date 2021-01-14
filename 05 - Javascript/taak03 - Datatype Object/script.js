6051597ec59b932c0c56459c7b00336255f27e27
ImranKaouch <2122928@talnet.nl>
2021-01-14 16:25:58 +0100

    taak 04

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,9 @@
+let student = {
+voornaam: "imran",
+achternaam: "Kaouch",
+leeftijd: "16",
+ikBenStemGerechtig: "False",
+ikMagLegaalAlcoholKopen: "False",
+ikMagBrommerRijden: "True"
+}
+console.log(student.leeftijd);
