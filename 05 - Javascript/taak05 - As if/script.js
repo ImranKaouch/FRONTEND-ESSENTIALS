d8945fcee1a88daec453e0e5ac6dcbc7edd82d1f
ImranKaouch <2122928@talnet.nl>
2021-01-14 16:44:11 +0100

    taak 05

---
+++ 05 - Javascript/taak05 - As if/script.js
@@ -0,0 +1,7 @@
+let mijnLeeftijd="16";
+let stemGerechtigeleeftijd= "18";
+
+if (mijnLeeftijd == stemGerechtigeleeftijd) {
+    console.log ("Ik ben niet stem gerechtig.");
+}
+alert ("Jij ben niet stemgerechtig");
