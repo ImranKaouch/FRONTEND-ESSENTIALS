0d7b04bfd4ac070c980b241215d1309d0616b6f2
ImranKaouch <2122928@talnet.nl>
2021-01-16 16:17:50 +0100

    taak 06

---
+++ 05 - Javascript/taak06 - If  something else/script.js
@@ -0,0 +1,9 @@
+let stopLichtKleur = "rood";
+
+if (stopLichtKleur == false) {
+console.log("Ik moet even wachten");
+}
+else{
+    console.log ("Ik rijd lekker door");
+}
+alert ("Jij moet stoppen voor het stoplicht");
