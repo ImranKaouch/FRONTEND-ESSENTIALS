e13fbfa6cbb967cd969318b451ad5699b0bb8ae3
ImranKaouch <2122928@talnet.nl>
2021-01-18 12:39:35 +0100

    taak 08

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -7,3 +7,12 @@ console.log(window.document);
 
 
 // document.querySelector("#poster1");
+
+var elem = document.querySelector ('#poster1');
+elem.style.backgroundColor = 'purple';
+
+var elem = document.querySelector ('#poster2');
+elem.style.backgroundColor = 'blue';
+
+var elem = document.querySelector ('#poster3');
+elem.style.backgroundColor = 'red';
---
+++ 05 - Javascript/taak08 - To let or not to let/script.js
@@ -0,0 +1,8 @@
+let codingIsCool = 10;
+codingIsCool = 11;
+console.log (codingIsCool);
+
+let huidigJaartal = 2021;
+let geboorteJaar = 2004;
+let mijnLeeftijd = 17;
+console.log (mijnLeeftijd = huidigJaartal - geboorteJaar);
