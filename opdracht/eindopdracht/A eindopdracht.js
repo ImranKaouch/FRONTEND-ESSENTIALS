88922505a52b36412ce951ff2e560b8ac5f62598
ImranKaouch <2122928@talnet.nl>
2021-02-05 13:27:42 +0100

    eindopdracht

---
+++ opdracht/eindopdracht/A eindopdracht.js
@@ -0,0 +1,15 @@
+let bezoeker= {
+     voornaam:"",
+     achternaam:"",
+     email:"",
+     leeftijd:"",
+     woonplaats:""
+}
+console.log(bezoeker);
+
+prompt("Wat is je voonaam?");
+prompt("Wat is je achternaam?");
+prompt("Wat is je email?");
+prompt ("Wat is je leeftijd?")
+prompt("Wat is je woonplaats?");
+
